01f73f4450725b16776e6cc87f7010ad3be5b250
tbcrawford <crawfordt.014@gmail.com>
2017-07-28 17:00:06 -0400

    fix colors. stupid lingering commas..

--- test/colors/colors.js
+++ test/colors/colors.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $.getJSON('/test/colors/color.json', function(colors, textStatus) {
         var swatches = "";
         for (let prop in colors) {
+            swatches += '<h1>' + prop + '</h1>'
             swatches += '<div class"swatches">';
             for (let color in colors[prop]) {
                 var hex = colors[prop][color];
@@ -13,5 +14,15 @@ $(document).ready(function() {
             swatches += '</div>';
         }
         $('body').append(swatches);
+
+        // Small hack for displaying things nicely
+        $('.off-white > span').addClass('inverted');
+
+
+        // This gets the text I want
+        $('.deep-blue span:first-child').text();
+
+        // Refer here:
+        //   https://stackoverflow.com/questions/22581345/click-button-copy-to-clipboard-using-jquery
     });
 });
